a8ea833b27883d6536694d14c9e44584d58e3e0b
Matheus Andrade <matheusaggioro@hotmail.com>
2021-04-27 19:31:53 -0300

    Fix alteração código

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './Exe';
+import Exe from './Exe';
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Exe />
   </React.StrictMode>,
   document.getElementById('root')
 );
