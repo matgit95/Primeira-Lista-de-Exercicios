0f106efe18b49f0b1fe45fa7f8e49942521309a1
Matheus <matheusaggioro@hotmail.com>
2021-04-27 16:27:11 -0300

    Primeiro commit

---
+++ src/Exe.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import './Exe.css';
+
+function Exe() {
+
+  const [nome, Nome] = useState("")
+  const [telefone, Telefone] = useState("")
+
+  return (
+    <div className="Exe">
+      <form>
+        <div>
+          <p>
+            Digite seu nome:
+          </p>
+          <input
+            value={nome}
+            onChange={e => Nome(e.target.value)}
+            placeholder="Nome"
+          />
+        </div>
+        <div>
+          <p>
+            Digite o telefone:
+          </p>
+          <input
+            value={telefone}
+            onChange={e => Telefone(e.target.value)}
+            placeholder="Telefone"
+          />
+        </div>
+      </form>
+      <div className="Central_result">
+        <div className="result">
+          <p>{nome}</p>
+          <p>{telefone}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Exe;
